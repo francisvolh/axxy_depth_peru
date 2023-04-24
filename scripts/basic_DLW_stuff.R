5af2231a006cc7fae13251b81821d6a7c180a731
Francis van Oordt <francis.vanoordtlahoz@mail.mcgill.ca>
2023-04-24 18:40:25 -0400

    updates to extract r values

--- scripts/basic_DLW_stuff.R
+++ scripts/basic_DLW_stuff.R
@@ -1,5 +1,6 @@
+library(tidyverse)
 
-init00<-read.csv("C:/Users/franc/OneDrive - McGill University/Documents/McGill/00Res Prop v2/Chap 1 - DLW axxy/dlw machine results/2021-10-15/initials.csv")
+init00<-read.csv("C:/Users/francis van oordt/OneDrive - McGill University/Documents/McGill/00Res Prop v2/Chap 1 - DLW axxy/dlw machine results/2021-10-15/initials.csv")
 
 head(init00)
 
@@ -11,13 +12,27 @@ summary_init<-init00 %>%
   )
 
 
-boxes<-read.csv("C:/Users/franc/OneDrive - McGill University/Documents/McGill/00Res Prop v2/Chap 1 - DLW axxy/boxes_DLW_2019_Boobies.csv")
+boxes<-read.csv("C:/Users/francis van oordt/OneDrive - McGill University/Documents/McGill/00Res Prop v2/Chap 1 - DLW axxy/boxes_DLW_2019_Boobies.csv")
 head(boxes)
 
 boxes_Init<-merge(boxes, summary_init, by.x="BirdBox", by.y="birdID", all=TRUE)
 
 plot(boxes_Init$mass01,boxes_Init$initValD)
 abline(lm(boxes_Init$initValD~boxes_Init$mass01))
+dlw.lm1<-lm(boxes_Init$initValD~boxes_Init$mass01)
+summary(
+  lm(boxes_Init$initValD~boxes_Init$mass01)
+  )
+
+sqrt(summary(dlw.lm1)$adj.r.squared)
+
 
 plot(boxes_Init$mass02,boxes_Init$initValD)
 abline(lm(boxes_Init$initValD~boxes_Init$mass02))
+
+summary(
+  lm(boxes_Init$initValD~boxes_Init$mass02)
+)
+dlw.lm2 <- lm(boxes_Init$initValD~boxes_Init$mass02)
+
+sqrt(summary(dlw.lm2)$adj.r.squared)
