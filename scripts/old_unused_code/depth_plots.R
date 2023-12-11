baba183caf6daff26d2dd60d4699f1fd67797ce9
Francis van Oordt <francis.vanoordtlahoz@mail.mcgill.ca>
2023-12-11 17:47:51 -0500

    moving old scripts

--- scripts/old_unused_code/depth_plots.R
+++
@@ -1,9 +0,0 @@
-#simple depth plots with threshold line
-
-for (i in unique(merged.tdr.gps$dep_id)) {
-  A<-merged.tdr.gps[which(merged.tdr.gps$dep_id == i),]
-  plot(A$time, A$depth*(-1), type = "l")
-  abline(h=-0.5, col = "red")
-  title(i)
-  readline('next')
-}
